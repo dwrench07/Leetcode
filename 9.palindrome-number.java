56328c6a49265c9d69dfbcc33e9d8a8e5dce263d
DWrench <dheerajreddynannuri1709@gmail.com>
2023-09-25 10:05:42 -0400

    Check palindrome by converting to string

--- 9.palindrome-number.java
+++ 9.palindrome-number.java
@@ -10,21 +10,34 @@ class Solution {
         /*
          * if x < 0 , return false
          * Solution 1: Convert to string
-         * 
+         * if the length is even:
+         * compare first to last
+         * till the half
+         * if the length is odd:
+         * compare till the middle, and leave
+         * the middle number.
          * Solution 2: Solve as a number
-         * 
+         * General method using the while
          */
-        int initial = x;
-        int reverse = 0;
         if (x < 0)
             return false;
-        else {
-            while (x > 0) {
-                reverse = reverse * 10 + x % 10;
-                x = x / 10;
+        String input = String.valueOf(x);
+        int counter = 0;
+        while (counter <= (input.length() / 2 - 1)) {
+            if (input.charAt(counter) == (input.charAt(input.length() - 1 - counter))) {
+                counter++;
+            } else {
+                return false;
             }
         }
-        return initial == reverse;
+        return true;
+        // int initial = x;
+        // int reverse = 0;
+        // while (x > 0) {
+        // reverse = reverse * 10 + x % 10;
+        // x = x / 10;
+        // }
+        // return initial == reverse;
     }
 }
 // @lc code=end
