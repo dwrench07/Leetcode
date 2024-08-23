b848f9e1ad86f7d89ab0e488cc32cd6690b6fa47
dwrench07 <45442264+dwrench07@users.noreply.github.com>
2024-08-22 23:03:26 -0700

    Update 28.find-the-index-of-first-occurence-in-string.ts

--- 28.find-the-index-of-first-occurence-in-string.ts
+++ 28.find-the-index-of-first-occurence-in-string.ts
@@ -1,17 +1,22 @@
 function strStr(haystack: string, needle: string): number {
+    if(needle.length > haystack.length || needle.length === 0)
+        return -1;
     let needleIndex: number = 0;
     let output: number = 0;
-    for(let index=0; index < haystack.length; index++){
-        if(haystack[index] == needle[needleIndex] && needleIndex < needle.length){
-            needleIndex++;
+    for(let index=0; index<haystack.length; index++){
+        if(haystack[index] === needle[needleIndex] && needleIndex < needle.length ){
             output = index - needleIndex;
+            needleIndex++;
         }else{
-            needleIndex = 0;
             output = -1;
+            index = index - needleIndex
+            needleIndex = 0;
         }
 
-        if(needle.length-1 == needleIndex)
+        if(needle.length === needleIndex)
             break;
+        else
+            output = -1
     }
     return output;
 };
