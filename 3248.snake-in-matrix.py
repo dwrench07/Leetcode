4e3b97e8f3dceee8e5a4c9c0b4b97c5ab7a07fec
Dheeraj Nannuri <dnannuri@amazon.com>
2025-12-04 01:05:30 -0800

    3248.snake-in-matrix.py - optimized

--- 3248.snake-in-matrix.py
+++ 3248.snake-in-matrix.py
@@ -13,19 +13,19 @@ class Solution(object):
         :rtype: int
         """
         directions = {
-            "LEFT": [0,-1],
-            "DOWN": [1,0],
-            "RIGHT": [0,1],
-            "UP": [-1,0]
+            "LEFT": -1,
+            "DOWN": n,
+            "RIGHT": 1,
+            "UP": -n
         }
 
-        current_position = [0,0]
-        for index in commands:
-            current_position = [current_position[0] + directions[index][0], current_position[1] + directions[index][1]]
+        current_position = 0
+        for command in commands:
+            current_position = current_position + directions[command]
 
-        return current_position[0]*n + current_position[1]
+        return current_position
 
 # @lc code=end
 
-# Time start: 12:32AM
-# Time end: 1:00AM
+# Time start: 01:02AM
+# Time end: 01:04AM
